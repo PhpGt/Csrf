19551b2ab28256309c47536ce76ad956a7aaead6
James Fellows <james.fellows@finanscapes.com>
2016-03-12 17:47:11 +0000

    Add phpdocs.
    Also added a composer script entry so the docs can be generated using
    './composer.phar docs'.
    
    NOTE: Phpdocumentor does not fully support PHP7 yet, so the docs are
    not generated for anything other than the exceptions at the moment.
    It looks like they're planning a release in the next few days that
    will.  Leaving ticket #22 open until that happens because until then
    we won't know whether everything is rendering properly!

--- src/HTMLDocumentProtector.php
+++ src/HTMLDocumentProtector.php
@@ -5,10 +5,26 @@ use phpgt\dom\HTMLDocument;
 
 class HTMLDocumentProtector
 {
+    /**
+     * @var string The name to be used for the hidden html input field used
+     * to store the token in each form
+     */
     public static $TOKEN_NAME = "csrf-token";
     private $doc;
     private $tokenStore;
 
+    /**
+     * HTMLDocumentProtector constructor.
+     *
+     * @param            $html       string|HTMLDocument The html document
+     *                               whose forms should be injected with CSRF
+     *                               tokens.  This can either be a
+     *                               \phpgt\dom\HTMLDocument or anything that
+     *                               can be used to construct one (such as
+     *                               string).
+     * @param TokenStore $tokenStore The TokenStore implementation to be used
+     *                               for generating and storing tokens.
+     */
     public function __construct($html, TokenStore $tokenStore)
     {
         $this->tokenStore = $tokenStore;
@@ -20,6 +36,9 @@ class HTMLDocumentProtector
         }
     }
 
+    /**
+     * Inject a CSRF token into each form in the html page.
+     */
     public function protectAndInject()
     {
         $forms = $this->doc->forms;
@@ -37,7 +56,14 @@ class HTMLDocumentProtector
         }
     }
 
-    public function getHTMLDocument() : \phpgt\dom\HTMLDocument
+    /**
+     * Retrieve the injected html.
+     *
+     * @return HTMLDocument Note that this can be used as-is, or if you just
+     * need to access the html as a string, call the HTMLDocument->saveHTML()
+     * method.
+     */
+    public function getHTMLDocument() : HTMLDocument
     {
         return $this->doc;
     }
--- src/TokenStore.php
+++ src/TokenStore.php
@@ -1,17 +1,43 @@
 <?php
 namespace phpgt\csrf;
 
+use phpgt\csrf\exception\CSRFTokenInvalidException;
 use phpgt\csrf\exception\CSRFTokenMissingException;
+use phpgt\csrf\exception\CSRFTokenSpentException;
 use RandomLib\Factory;
 use SecurityLib\Strength;
 
+/**
+ * Class TokenStore
+ * Extend this base class to create a store for CSRF tokens.  The core
+ * functionality of generating the tokens is provided by the base class, but
+ * can be overridden.
+ *
+ * @package phpgt\csrf
+ */
 abstract class TokenStore
 {
+    /**
+     * @var int|null The maximum number of tokens to be retained.
+     */
     public static $MAX_TOKENS  = 1000;
     private static $strength    = Strength::MEDIUM;
     private static $tokenLength = 64;
     private $tokenGenerator;
 
+    /**
+     * TokenStore constructor.
+     *
+     * @see TokenStore::$MAX_TOKENS the class property storing the maximum
+     * tokens limit.
+     *
+     * @param int|null $maxTokens An optional limit to the number of valid
+     *                            tokens the TokenStore will retain.
+     *                            If not specified, an unlimited number of
+     *                            tokens will be retained (which is probably
+     *                            fine unless you have a very, very busy site
+     *                            with long-running sessions).
+     */
     public function __construct(int $maxTokens = null)
     {
         if ($maxTokens !== null) {
@@ -23,13 +49,38 @@ abstract class TokenStore
             new Strength(self::$strength));
     }
 
+    /**
+     * Generate a new token.  NOTE: This method does NOT store the token.  To
+     * do so
+     *
+     * @see TokenStore::saveToken() for storing a generated token.
+     *
+     * @return string The newly generated token.
+     */
     public function generateNewToken() : string
     {
         return $this->tokenGenerator->generateString(
             self::$tokenLength);
     }
 
-    public function processAndVerify()
+    /**
+     * If a $_POST global exists, check that it contains a token and that the
+     * token is valid.  The name the token is stored-under is contained in
+     * @see HTMLDocumentProtector::$TOKEN_NAME.
+     *
+     * NOTE that the method will always either return true if everything is
+     * ok, or it will throw an exception if not.
+     *
+     * @return bool True if the request should be expected.
+     * @throws CSRFTokenMissingException There's a $_POST request present but no
+     * token present
+     * @throws CSRFTokenInvalidException There's a token included on the $_POST,
+     * but its value is invalid.
+     * @throws CSRFTokenSpentException  There's a token included on the
+     * $_POST but it has already been consumed by a previous request.  @see
+     * TokenStore::verifyToken().
+     */
+    public function processAndVerify() : bool
     {
         // expect the token to be present on ALL post requests
         if (!empty($_POST)) {
@@ -40,11 +91,38 @@ abstract class TokenStore
             $this->verifyToken($_POST[ HTMLDocumentProtector::$TOKEN_NAME ]);
             $this->consumeToken($_POST[ HTMLDocumentProtector::$TOKEN_NAME ]);
         }
+
+        return true;
     }
 
+    /**
+     * Save a token as valid for later verification
+     *
+     * @param string $token The token to be stored
+     */
     abstract public function saveToken(string $token);
 
+    /**
+     * Mark a token as "used".
+     *
+     * @param string $token The token to consume
+     */
     abstract public function consumeToken(string $token);
 
+    /**
+     * Checks that the token is valid (i.e. exists and has not been consumed
+     * already).
+     *
+     * @param string $token  The token to be checked.
+     *
+     * @throws CSRFTokenInvalidException The token is invalid (i.e. is not
+     * contained within the store).
+     * @throws CSRFTokenSpentException The token has been consumed already. This
+     * scenario might be handled differently by the web app in case the user
+     * pressed submit twice in quick succession - instructing them
+     * to refresh the page and resubmit their form for example.
+     *
+     * @return bool true if the token is valid
+     */
     abstract public function verifyToken(string $token) : bool;
 }#
