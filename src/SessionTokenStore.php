27280dc03fe44d12856c1ebd481692e3ba924dab
Greg Bowler <greg.bowler@g105b.com>
2018-08-14 13:48:09 +0100

    Implement SessionTokenStore

--- src/SessionTokenStore.php
+++ src/SessionTokenStore.php
@@ -3,33 +3,52 @@ namespace Gt\Csrf;
 
 use Gt\Csrf\Exception\CsrfTokenInvalidException;
 use Gt\Csrf\Exception\CsrfTokenSpentException;
+use Gt\Session\SessionStore;
 
 class SessionTokenStore extends TokenStore {
-	/**
-	 * Save a token as valid for later verification.
-	 */
+	const SESSION_KEY = "tokenList";
+
+	/** @var SessionStore */
+	protected $session;
+
+	public function __construct(SessionStore $session, int $maxTokens = null) {
+		$this->session = $session;
+
+		parent::__construct($maxTokens);
+	}
+
 	public function saveToken(string $token):void {
-		// TODO: Implement saveToken() method.
+		$tokenList = $this->session->get(self::SESSION_KEY) ?? [];
+		$tokenList[$token] = null;
+
+		while(count($tokenList) > self::$MAX_TOKENS) {
+			array_shift($tokenList);
+		}
+
+		$this->session->set(self::SESSION_KEY, $tokenList);
 	}
 
-	/**
-	 * Mark a token as "used".
-	 */
-	public function consumeToken(string $token):void {
-		// TODO: Implement consumeToken() method.
-	}
-
-	/**
-	 * Check that the token is valid (i.e. exists and has not been consumed already).
-	 *
-	 * @throws CsrfTokenInvalidException The token is invalid (i.e. is not
-	 * contained within the store).
-	 * @throws CsrfTokenSpentException The token has been consumed already. This
-	 * scenario might be handled differently by the web app in case the user
-	 * pressed submit twice in quick succession - instructing them
-	 * to refresh the page and resubmit their form for example.
-	 */
 	public function verifyToken(string $token):bool {
-		// TODO: Implement verifyToken() method.
+		$tokenList = $this->session->get(self::SESSION_KEY) ?? [];
+
+		if(!array_key_exists($token, $tokenList)) {
+			throw new CsrfTokenInvalidException(
+				$token
+			);
+		}
+		elseif(!is_null($tokenList[$token])) {
+			throw new CsrfTokenSpentException(
+				$token,
+				$tokenList[$token]
+			);
+		}
+
+		return true;
+	}
+
+	public function consumeToken(string $token):void {
+		$tokenList = $this->session->get(self::SESSION_KEY) ?? [];
+		$tokenList[$token] = time();
+		$this->session->set(self::SESSION_KEY, $tokenList);
 	}
 }
