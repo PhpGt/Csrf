08da917028ababe675728914462b74d852e33b8f
Greg Bowler <greg.bowler@g105b.com>
2018-12-04 18:05:30 +0000

    Test when token is spent

--- test/unit/SessionTokenStoreTest.php
+++ test/unit/SessionTokenStoreTest.php
@@ -2,6 +2,7 @@
 namespace Gt\Csrf\Test;
 
 use Gt\Csrf\Exception\CsrfTokenInvalidException;
+use Gt\Csrf\Exception\CsrfTokenSpentException;
 use Gt\Csrf\SessionTokenStore;
 use Gt\Session\SessionStore;
 use PHPUnit\Framework\TestCase;
@@ -104,6 +105,31 @@ class SessionTokenStoreTest extends TestCase {
 
 		self::expectException(CsrfTokenInvalidException::class);
 		/** @var SessionStore $session*/
+		$sessionTokenStore = new SessionTokenStore($session);
+		$sessionTokenStore->verifyToken($tokenToCheckFor);
+	}
+
+	public function testVerifyTokenSpent() {
+		$tokenToCheckFor = uniqid("token-");
+		$existingTokens = [];
+		for($i = 0; $i < 9; $i++) {
+			$key = uniqid("token-");
+			$existingTokens[$key] = null;
+		}
+
+// Add the token to check for in the middle of the existing tokens.
+		$existingTokens = array_merge(
+			array_slice($existingTokens, 0, 4),
+			[$tokenToCheckFor => time()],
+			array_slice($existingTokens, 4)
+		);
+
+		$session = self::createMock(SessionStore::class);
+		$session->method("get")
+			->willReturn($existingTokens);
+
+		self::expectException(CsrfTokenSpentException::class);
+		/** @var SessionStore $session */
 		$sessionTokenStore = new SessionTokenStore($session);
 		$sessionTokenStore->verifyToken($tokenToCheckFor);
 	}
