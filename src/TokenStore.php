1317c026302e20f26d43d73c5b30cb5e3b0fc5f8
Greg Bowler <greg.bowler@g105b.com>
2019-01-30 09:23:54 +0000

    Use asArray function for phpgt compatibility

--- src/TokenStore.php
+++ src/TokenStore.php
@@ -84,8 +84,8 @@ abstract class TokenStore {
 	public function processAndVerify($postData):void {
 // Expect the token to be present on ALL post requests.
 		if(!is_array($postData)
-		&& is_callable([$postData, "toArray"])) {
-			$postData = call_user_func([$postData, "toArray"]);
+		&& is_callable([$postData, "asArray"])) {
+			$postData = call_user_func([$postData, "asArray"]);
 		}
 
 		if(!empty($postData)) {
